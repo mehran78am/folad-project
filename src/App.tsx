07b293ca6d050b3bdc45e4a1cf03e4696f2be447
mehran <mehran78am.gmail.com>
2025-03-05 18:17:00 +0330

    fetch data faild

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,24 @@ import About from "./components/About";
 import Footer from "./components/Footer";
 import Article from "./components/Article";
 import MobileBar from "./components/MobileBar";
-function App() {
+import Test from "./components/Test";
+import { UserProvider } from "./components/UserContext";
+const App = () => {
   return (
-    <div className="container mx-auto px-4 py-1 overflow-x-hidden select-none relative">
-      <Navbar />
-      <SliderTop />
-      <Gallery />
-      <Article />
-      <Contact />
-      <About />
-      <Footer />
-      <MobileBar />
-    </div>
+    <UserProvider>
+      <div className="container mx-auto px-4 py-1 overflow-x-hidden select-none relative">
+        <Navbar />
+        <SliderTop />
+        <Gallery />
+        <Article />
+        <Contact />
+        <About />
+        <Footer />
+        <MobileBar />
+        <Test/>
+      </div>
+    </UserProvider>
   );
-}
+};
 
 export default App;
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,84 +1,25 @@
-// import { useState } from "react";
-// import Angel from "../assets/img/Angel-300x225.webp";
-// import Beam from "../assets/img/Beam-300x200.webp";
-// import ColdRolled from "../assets/img/Cold-Rolled-300x169.webp";
-// import HotRolled from "../assets/img/Hot-Rolled-300x200.webp";
-// const images = [Angel, Beam, ColdRolled, HotRolled];
+// UserList.tsx
+import React from 'react';
+import { useUserContext } from './UserContext';
 
-// const Test = () => {
-//   const [currentIndex, setCurrentIndex] = useState(0);
+const Test: React.FC = () => {
+    const { users, loading, error } = useUserContext();
 
-//   const nextSlide = () => {
-//     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-//   };
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>{error}</div>;
 
-//   const prevSlide = () => {
-//     setCurrentIndex(
-//       (prevIndex) => (prevIndex - 1 + images.length) % images.length
-//     );
-//   };
-
-//   return (
-//     <div className="relative w-full overflow-hidden mt-10">
-//       <div
-//         className="flex transition-transform duration-500"
-//         style={{ transform: `translateX(-${currentIndex * 250}px)` }}
-//       >
-//         {images.map((image, index) => (
-//           <img
-//             key={index}
-//             src={image}
-//             alt={`Slide ${index}`}
-//             className="w-250 h-auto"
-//           />
-//         ))}
-//       </div>
-//       <button
-//         onClick={prevSlide}
-//         className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow"
-//       >
-//         Prev
-//       </button>
-//       <button
-//         onClick={nextSlide}
-//         className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow"
-//       >
-//         Next
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Test;
-
-// src/Slider.tsx
-import React, { useEffect, useState } from "react";
-
-const items = [1, 2, 3, 4, 5];
-
-const Slider: React.FC = () => {
-  const [startIndex, setStartIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setStartIndex((prevIndex) => (prevIndex + 1) % (items.length - 1));
-    }, 5000); // تغییر هر 3 ثانیه
-
-    return () => clearInterval(interval); // پاک کردن interval هنگامUnmount
-  }, []);
-
-  return (
-    <div className="flex space-x-4">
-      {items.slice(startIndex, startIndex + 2).map((item) => (
-        <div
-          key={item}
-          className="w-32 h-32 bg-blue-500 flex items-center justify-center text-white rounded-lg"
-        >
-          {item}
-        </div>
-      ))}
-    </div>
-  );
+    return (
+        <ul>
+            {users.map(user => (
+                <li key={user.id}>
+                    <img src={user.imageUrl} alt={user.username} style={{ width: '50px', height: '50px' }} />
+                    <h2>{user.username}</h2>
+                    <p>Email: {user.email}</p>
+                    <p>Phone: {user.phone}</p>
+                </li>
+            ))}
+        </ul>
+    );
 };
 
-export default Slider;
+export default Test;
